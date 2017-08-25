b404e45e3fbdd99142a39d5a87243b9d6dd1baac
Etienne <etienne.cuierrier@usherbrooke.ca>
2017-08-25 15:03:37 -0400

    add a function to calculate for a trajectory

--- dataAnalysis/endtoend_distance.py
+++ dataAnalysis/endtoend_distance.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 """
-This function will read a lammps output to compute the end to end distances (Rf if averaged) of the backbone.
+These function can be used to calculate the average end to end distances of a backbone from a lammmps output.
 
 Usage:
 # dump_dataframe must be in pythonpath or working directory
 from endtoend_distance import rf
-distances = rf(filename,atoms_per_polyer,number_of_chains)
+rf,rf_std = rf(first_frame=-1000, last_frame=-1, trajectory_step=10,atoms_per_polymer=184, number_of_chains=100)
 
 Requirement:
 numpy
@@ -15,6 +15,7 @@ scipy
 
 Limitations:
 Coordinates must be unwrapped (ex:xu,yu,zu)
+Each dump must be a file
 
 TODO:
 Function to read a trajectory from a single file
@@ -24,8 +25,9 @@ from dump_dataframe import read_dump
 import numpy as np
 import pandas as pd
 from scipy.spatial.distance import pdist
+from glob import glob
 
-def rf(filename,atoms_per_polymer=184,number_of_chains=100):
+def endtoend(filename, atoms_per_polymer, number_of_chains):
     """
     Function to calculate the end to end distances of each polymer chains from a dump.
 
@@ -39,18 +41,58 @@ def rf(filename,atoms_per_polymer=184,number_of_chains=100):
     ----
     endtoend_dists(array): Numpy array with the end-to-end distance for each chains
     """
-    #Read the dump, coordinates must be unwrapped
+    # Read the dump, coordinates must be unwrapped
     dump = read_dump(filename, wrap=False)
-    #Select only the useful columns
-    wanted_columns = ["xu","yu","zu"]
+    # Select only the useful columns
+    wanted_columns = ["xu", "yu", "zu"]
     rf_df = dump["atom_df"][wanted_columns]
-    #Create an empty array which will contains the distances
-    endtoend_dists=np.zeros(number_of_chains)
+    # Create an empty array which will contains the distances
+    endtoend_dists = np.zeros(number_of_chains)
 
-    i=0
+    i = 0
     while i < number_of_chains:
-        #Calculate the distance betwenn the fist and the last atoms in the backbone
-        endtoend_dists[i]=pdist(rf_df.loc[[1+atoms_per_polymer*i,atoms_per_polymer+atoms_per_polymer*i]])
-        i+=1
+        # Calculate the distance between the fist and the last atoms in the
+        # backbone
+        endtoend_dists[i] = pdist(
+            rf_df.loc[[1 + atoms_per_polymer * i, atoms_per_polymer + atoms_per_polymer * i]])
+        i += 1
     return endtoend_dists
-rf()
+
+
+def rf(first_frame=-1000, last_frame=-1, trajectory_step=10,atoms_per_polymer=184, number_of_chains=100):
+    """
+    Function to calculate the Rf of a lammps trajectory.
+
+    Args:
+    ----
+    first_frame(int): The first frame desired in the trajectory
+    last_frame(int): The frame to stop
+    trajectory_step(int): calculate only for each # of files
+    atoms_per_polymer(int): The number of atoms in the polymer chain
+    number_of_chains(int): The number of chains in the system
+
+    Returns:
+    ----
+    Rfmean(float): The average end to end distances in the trajectory
+    Rfstd(float): The standard deviation of the Rf
+    """
+    # List of all the dump in the trajectory
+    complete_trajectory = glob("*dump*")
+
+    # sort the list according to the number in the filename
+    complete_trajectory.sort(key=lambda f: int(filter(str.isdigit, f)))
+
+    # consider only the desired frames
+    desired_trajectory = complete_trajectory[first_frame:last_frame:trajectory_step]
+
+    #create a empty numpy array to contains the end to end distances for each chain (columns)
+    #for each step (time)
+    rf = np.zeros((len(desired_trajectory),number_of_chains))
+    i=0
+    # for each file in the trajectory
+    for f in desired_trajectory:
+        #calculate the end to end distances for each chain
+        rf[i] = endtoend(f, atoms_per_polymer, number_of_chains)
+        i+=1
+    #return the mean average distances with its standard deviation
+    return rf.mean(),rf.std()
