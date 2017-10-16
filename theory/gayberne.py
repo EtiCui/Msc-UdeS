33ffda00240194444a59661742dd166e737b324f
Etienne <etienne.cuierrier@usherbrooke.ca>
2017-10-16 11:07:08 -0400

    fixed for the correct key value

--- theory/gayberne.py
+++ theory/gayberne.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python2
 """This script will call LAMMPS to calculate and visualize (with matplotlib) the Gay-Berne potential
-for the side-by-side, end-to-end,tee and cross configurations for a uniaxial ellipsoid.
+for the side-by-side (ll), end-to-end(z),tee(T) and cross(X) configurations for a uniaxial ellipsoid.
 
 Note:
 If the default intermolecular distances for a configuration are too far from it's minimum,
@@ -8,7 +9,7 @@ some tuning might be required in the move_molecule function
 Usage:
 #the script must be in python path or working directory
 from gayberne import gb
-gb = gb(width=1, length=3, sigma=1, cross_depth=3, end_depth=1, nu=1, mu=2)
+gb = gb(width=1, length=3, sigma=1, X_depth=3, z_depth=1, nu=1, mu=2,x_range=150)
 lammps_df = gb.lammps_gb()
 gb.visualize(lammps_df=lammps_df)
 
@@ -22,7 +23,6 @@ system.data file (the defined parameters will be overwritten)
 
 TODO:
 The system.data file should not be required
-
 """
 import numpy as np
 import math
@@ -34,20 +34,22 @@ import matplotlib.lines as mlines
 
 
 class gb():
-    def __init__(self, width, length, sigma, cross_depth, end_depth, nu, mu, cutoff=30):
+    def __init__(self, width, length, sigma, X_depth, z_depth, nu, mu, cutoff=30,x_range=150):
         """ Definition of the parameters and initialization"""
         self.df = pd.DataFrame()
         # configuration with the color for the graph: red, green, blue, yellow
-        self.colors = {"side": "r", "cross": "g", "end": "b", "tee": "y"}
+        self.colors = {"ll": "r", "X": "g", "z": "b", "T": "y"}
         # parameters definition
         self.width = width
         self.length = length
         self.sigma = sigma
-        self.cross_depth = cross_depth
-        self.end_depth = end_depth
+        self.X_depth = X_depth
+        self.z_depth = z_depth
         self.nu = nu
         self.mu = mu
         self.cutoff = cutoff
+        # range for the graph
+        self.x_range = x_range
 
     def lammps_gb(self):
         """ This function will call lammps to calculate the Gay-Berne potential for a uniaxial ellipsoid
@@ -70,9 +72,9 @@ class gb():
         lmp.command("set atom 1 quat 1 0 0 0")
         lmp.command("set atom 2 quat 1 0 0 0")
         # Define the gay-berne pair_coeff
-        lmp.command("pair_coeff 1 1 1.0 %f %f %f %f %f %f %f" % (self.sigma, self.cross_depth, self.cross_depth,
-                                                                 self.end_depth, self.cross_depth,
-                                                                 self.cross_depth, self.end_depth))
+        lmp.command("pair_coeff 1 1 1.0 %f %f %f %f %f %f %f" % (self.sigma, self.X_depth, self.X_depth,
+                                                                 self.z_depth, self.X_depth,
+                                                                 self.X_depth, self.z_depth))
 
         # function to move the first ellipsoid. direction: 0 is x, 1 is y and 2
         # is z.
@@ -82,7 +84,7 @@ class gb():
             pot_tot = []
             position = []
             i = 0
-            while i < 150:
+            while i < self.x_range:
                 xc = lmp.gather_atoms("x", 1, 3)
                 lmp.command("run 0")
                 pot = lmp.extract_compute("thermo_pe", 0, 0)
@@ -92,46 +94,46 @@ class gb():
                 lmp.scatter_atoms("x", 1, 3, xc)
                 i = i + 1
             position = pd.DataFrame(
-                {str(configuration) + "_distance": position})
+                {"MD_"+ str(configuration) + "_distance": position})
             pot_tot = pd.DataFrame(
-                {str(configuration) + "_energy": pot_tot})
+                {"MD_" + str(configuration) + "_energy": pot_tot})
             self.df = pd.concat([self.df, position, pot_tot], axis=1)
 
         for configuration in self.colors:
             # reinitialze the position of the first atom
             lmp.command("set atom 1 x 0 y 0 z 0")
-            if configuration == "side" or "end":
-                # No rotatio for side and end configuration
+            if configuration == "ll" or "z":
+                # No rotatio for ll and end configuration
                 lmp.command("set atom 1 quat 1 0 0 0")
-                if configuration == "side":
+                if configuration == "ll":
                     # move in x direction for side-by-side
                     direction = 0
                     xc = lmp.gather_atoms("x", 1, 3)
                     # Can be changed if the values are too far from the minimum
-                    xc[direction] = xc[direction] + self.width / 1.5
+                    xc[direction] = xc[direction] + self.width/1.5
                     xc = lmp.scatter_atoms("x", 1, 3, xc)
                     move_molecule(configuration, direction)
-                if configuration == "end":
+                if configuration == "z":
                     # move in the z direction for end-to-end
                     direction = 2
                     xc = lmp.gather_atoms("x", 1, 3)
                     # Can be changed if the values are too far from the minimum
-                    xc[direction] = xc[direction] + self.length / 1.1
+                    xc[direction] = xc[direction] + self.length/1.1
                     xc = lmp.scatter_atoms("x", 1, 3, xc)
                     move_molecule(configuration, direction)
 
-            if configuration == "tee" or "cross":
+            if configuration == "T" or "X":
                 # The first ellipsoid is rotated by 90 degree in the x axis
                 lmp.command("set atom 1 quat 1 0  0 90")
-                if configuration == "cross":
+                if configuration == "X":
                     # the first ellipsoid is translated in the x direction
                     direction = 0
                     xc = lmp.gather_atoms("x", 1, 3)
                     # Can be changed if the values are too far from the minimum
-                    xc[direction] = xc[direction] + self.width / 1.5
+                    xc[direction] = xc[direction] + self.width/1.5
                     xc = lmp.scatter_atoms("x", 1, 3, xc)
                     move_molecule(configuration, direction)
-                if configuration == "tee":
+                if configuration == "T":
                     # Translation in the y axis
                     direction = 1
                     xc = lmp.gather_atoms("x", 1, 3)
@@ -140,6 +142,7 @@ class gb():
                     xc = lmp.scatter_atoms("x", 1, 3, xc)
                     move_molecule(configuration, direction)
         # close lammps
+        print("LAMMPS done")
         return self.df
         lmp.close()
 
@@ -150,9 +153,9 @@ class gb():
 
         for configuration in self.colors:
             # Remove values with a energy superior to 5 for the graph
-            df_graph = lammps_df.where(lammps_df[str(configuration) + "_energy"] < 5)
-            plt.plot(df_graph[str(configuration) + "_distance"].dropna(),
-            df_graph[str(configuration) + "_energy"].dropna(), label=str(configuration),
+	    df_graph = lammps_df.where(lammps_df["MD_"+str(configuration) + "_energy"] < 5)
+            plt.plot(df_graph["MD_"+str(configuration) + "_distance"].dropna(),
+            df_graph["MD_"+str(configuration) + "_energy"].dropna(), label=str(configuration),
             color=self.colors[configuration], ms=5, markevery=0.02)
             plt.ylabel(r"Potentiel ($\epsilon_0$)")
             plt.xlabel(r"Distance ($\sigma_0$)")
@@ -161,3 +164,6 @@ class gb():
         plt.tight_layout()
 
         plt.show()
+#gb = gb(width=1, length=3, sigma=1, X_depth=3, z_depth=1, nu=1, mu=2,x_range=150)
+#lammps_df = gb.lammps_gb()
+#gb.visualize(lammps_df=lammps_df)
